7b58bf2ca7db0997535efe724a94d07d83a4993e
Jagdish <jagdish.g@simformsolutions.com>
2022-07-29 12:02:33 +0530

    implemented api routes with POST and GET methods for feedback form in HomePage

---
+++ pages/api/feedback.js
@@ -0,0 +1,36 @@
+import fs from "fs";
+import path from "path";
+const buildFeedbackPath = () => {
+  return path.join(process.cwd(), "data", "feedback.json");
+};
+const extractFeedbackData = (filePath) => {
+  const fileData = fs.readFileSync(filePath);
+  return JSON.parse(fileData);
+};
+function handler(req, res) {
+  // try {
+
+  if (req.method === "POST") {
+    const enteredEmail = req.body.email;
+    const enteredFeedback = req.body.feedback;
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: enteredEmail,
+      feedback: enteredFeedback,
+    };
+
+    const filePath = buildFeedbackPath();
+    const data = extractFeedbackData(filePath);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedbackData(filePath);
+    res.status(200).json({ feedback: data });
+  }
+  // } catch {
+  //   (error) => console.log("error", error);
+  // }
+}
+export default handler;
