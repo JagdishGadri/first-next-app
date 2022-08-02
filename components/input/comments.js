ba5ed77f90e8add550c9a41fce66eec951f88562
Jagdish <jagdish.g@simformsolutions.com>
2022-08-02 18:48:29 +0530

    implemented notification pop-up using react context

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
 import classes from "./comments.module.css";
-
+import NotificationContext from "../../store/notification-context";
 function Comments(props) {
   const { eventId } = props;
+  const notificationCtx = useContext(NotificationContext);
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
@@ -27,7 +28,13 @@ function Comments(props) {
 
   function addCommentHandler(commentData) {
     // send data to API
-    console.log("commentData", commentData);
+
+    notificationCtx.showNotification({
+      title: "Sending Comment",
+      message: "Your comment is being stored in database",
+      status: "pending",
+    });
+
     fetch(`/api/comments/${eventId}`, {
       method: "POST",
       body: JSON.stringify(commentData),
@@ -35,8 +42,29 @@ function Comments(props) {
         "Content-Type": "application/json",
       },
     })
-      .then((res) => res.json())
-      .then((data) => console.log("recievedCommentData", data));
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return res.json().then((data) => {
+          throw new Error(data.message || "Something went wrong!");
+        });
+      })
+      .then((data) => {
+        console.log("recievedCommentData", data);
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Your Comment is Saved",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: "Oh no! Error occured",
+          message: error.message || "Try Again",
+          status: "error",
+        });
+      });
   }
 
   return (
--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import MainHeader from "./main-header";
+import Notification from "../ui/notification";
+import NotificationContext from "../../store/notification-context";
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+  console.log("motificationctx in layout", notificationCtx);
+
+  const activeNotification = notificationCtx.notification;
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={notificationCtx.notification.title}
+          message={notificationCtx.notification.message}
+          status={notificationCtx.notification.status}
+        />
+      )}
     </Fragment>
   );
 }
