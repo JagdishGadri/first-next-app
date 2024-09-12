223644969e79a343ceaafe6c23b2befafc329023
Charizard <jagdishjagdish752@gmail.com>
2024-09-12 11:27:53 +0530

    fix: Made env variable public for client side access

--- components/chat/message-input.tsx
+++ components/chat/message-input.tsx
@@ -17,7 +17,7 @@ function Input({
   const ws = useRef<WebSocket | null>(null);
   const [isNewMessageReceived, setIsNewMessageReceived] = useState(false);
   const connectToWebSocket = useCallback(async () => {
-    ws.current = new WebSocket(process.env.WS_URL);
+    ws.current = new WebSocket(process.env.NEXT_PUBLIC_WS_URL);
     ws.current.onopen = () => {
       if (ws.current)
         ws.current.send(
